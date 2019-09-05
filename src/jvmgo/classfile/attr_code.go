a6ce6427bd6b0de44948280550f2385dc81b6e89
ztex <zyex030640417@gmail.com>
2019-09-05 13:03:40 +0800

    Feat Implement the Code attribute which store info related to code

---
+++ src/jvmgo/classfile/attr_code.go
@@ -0,0 +1,97 @@
+package classfile
+
+/*
+A Code attribute contains the Java Virtual Machine instructions and auxiliary information for a method, including an instance initialization method or a class or interface initialization method (§2.9).
+*/
+/*
+Code_attribute {
+    u2 attribute_name_index;
+    u4 attribute_length;
+    u2 max_stack; // maximum depth of the operand stack of this method during execution
+    u2 max_locals;
+    u4 code_length;
+    u1 code[code_length];
+    u2 exception_table_length;
+    {   u2 start_pc;
+        u2 end_pc;
+        u2 handler_pc;
+        u2 catch_type;
+    } exception_table[exception_table_length];
+    u2 attributes_count;
+    attribute_info attributes[attributes_count];
+}
+*/
+type CodeAttribute struct {
+	cp             ConstantPool
+	maxStack       uint16
+	maxLocals      uint16
+	code           []byte // java machine code
+	exceptionTable []*ExceptionTableEntry
+	attributes     []AttributeInfo
+}
+
+func (self *CodeAttribute) readInfo(reader *ClassReader) {
+	self.maxStack = reader.readUint16()
+	self.maxLocals = reader.readUint16()
+	codeLength := reader.readUint32()
+	self.code = reader.readBytes(codeLength)
+	self.exceptionTable = readExceptionTable(reader)
+	self.attributes = readAttributes(reader, self.cp)
+}
+
+func (self *CodeAttribute) MaxStack() uint {
+	return uint(self.maxStack)
+}
+func (self *CodeAttribute) MaxLocals() uint {
+	return uint(self.maxLocals)
+}
+func (self *CodeAttribute) Code() []byte {
+	return self.code
+}
+func (self *CodeAttribute) ExceptionTable() []*ExceptionTableEntry {
+	return self.exceptionTable
+}
+
+// see: https://docs.oracle.com/javase/specs/jvms/se8/html/jvms-4.html#jvms-4.7.5
+/*
+start_pc and end_pc indicate the ranges in the code array at which the exception handler is active.
+*/
+/*
+handler_pc: indicates the start of the exception handler
+*/
+/*
+If the value of the catch_type item is nonzero, it must be a valid index into the constant_pool table
+*/
+type ExceptionTableEntry struct {
+	startPc   uint16
+	endPc     uint16
+	handlerPc uint16
+	catchType uint16
+}
+
+func readExceptionTable(reader *ClassReader) []*ExceptionTableEntry {
+	exceptionTableLength := reader.readUint16()
+	exceptionTable := make([]*ExceptionTableEntry, exceptionTableLength)
+	for i := range exceptionTable {
+		exceptionTable[i] = &ExceptionTableEntry{
+			startPc:   reader.readUint16(),
+			endPc:     reader.readUint16(),
+			handlerPc: reader.readUint16(),
+			catchType: reader.readUint16(),
+		}
+	}
+	return exceptionTable
+}
+
+func (self *ExceptionTableEntry) StartPc() uint16 {
+	return self.startPc
+}
+func (self *ExceptionTableEntry) EndPc() uint16 {
+	return self.endPc
+}
+func (self *ExceptionTableEntry) HandlerPc() uint16 {
+	return self.handlerPc
+}
+func (self *ExceptionTableEntry) CatchType() uint16 {
+	return self.catchType
+}
