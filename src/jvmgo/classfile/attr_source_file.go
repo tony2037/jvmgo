6eeb8bdd1945e92c5f5749b27ce438c8265d1ebe
ztex <zyex030640417@gmail.com>
2019-09-05 12:45:23 +0800

    Feat Implement The SourceFile attribute

---
+++ src/jvmgo/classfile/attr_source_file.go
@@ -0,0 +1,25 @@
+package classfile
+
+// The SourceFile attribute is an optional fixed-length attribute in the attributes table of a ClassFile structure
+/*
+@ attribute_length: The value of the attribute_length item of a SourceFile_attribute structure must be two.
+*/
+/*
+SourceFile_attribute {
+    u2 attribute_name_index;
+    u4 attribute_length;
+    u2 sourcefile_index;
+}
+*/
+type SourceFileAttribute struct {
+	cp              ConstantPool
+	sourceFileIndex uint16
+}
+
+func (self *SourceFileAttribute) readInfo(reader *ClassReader) {
+	self.sourceFileIndex = reader.readUint16()
+}
+
+func (self *SourceFileAttribute) FileName() string {
+	return self.cp.getUtf8(self.sourceFileIndex)
+}
